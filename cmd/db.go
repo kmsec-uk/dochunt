2481dad137b808cf9088e387246e25a2874bd4cb
kmsec-uk <kmsec-uk@dev.null>
2026-03-26 16:55:09 +0000

    processing pipeline

---
+++ cmd/db.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"database/sql"
+	json "encoding/json/v2"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/kmsec-uk/ccdocs/gdoc"
+	_ "modernc.org/sqlite"
+)
+
+// ensure a directory exists.
+func ensureDirExists(dir string) {
+	err := os.MkdirAll(dir, 0750)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+type DB struct {
+	db              *sql.DB
+	upsertDoc       *sql.Stmt
+	insertProcessed *sql.Stmt
+	existsProcessed *sql.Stmt
+}
+
+// query for inserting a document. sepearated out for readability
+var upsertQuery string = `
+INSERT INTO docs (
+	id, revision, sources, created,
+	page_title, og_title, og_description, og_image,
+	content, links, image_urls
+) VALUES (
+	?, ?, json_array(json(?)), ?,
+	?, ?, ?, ?,
+	?, json(?), json(?)
+)
+ON CONFLICT(id, revision) DO UPDATE SET
+	sources = json_insert(sources, '$[#]', json(?));`
+
+var dsn string = "?_pragma=journal_mode(WAL)&" +
+	"_pragma=foreign_keys(ON)&" +
+	"_pragma=busy_timeout(5000)&" +
+	"_pragma=synchronous(FULL)"
+
+func NewDB(dbPath string) (*DB, error) {
+	ensureDirExists(filepath.Dir(dbPath))
+	db, err := sql.Open("sqlite", "file:"+dbPath+dsn)
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS docs (
+		id TEXT NOT NULL,
+		revision INTEGER NOT NULL,
+		sources TEXT NOT NULL,
+		created TEXT NOT NULL,
+		page_title TEXT NOT NULL,
+		og_title TEXT NOT NULL,
+		og_description TEXT NOT NULL,
+		og_image TEXT NOT NULL,
+		content TEXT NOT NULL,
+		links TEXT NOT NULL,
+		image_urls TEXT NOT NULL,
+		PRIMARY KEY (id, revision)
+		);
+		CREATE TABLE IF NOT EXISTS processed (digest TEXT PRIMARY KEY);`)
+
+	if err != nil {
+		return nil, fmt.Errorf("setting up db: %w", err)
+	}
+	db.SetMaxOpenConns(1)
+	DB := &DB{db: db}
+
+	stmtInsertProcessed, err := db.Prepare(`INSERT INTO processed (digest) VALUES (?)`)
+	if err != nil {
+		return nil, err
+	}
+	DB.insertProcessed = stmtInsertProcessed
+
+	stmtExistsProcessed, err := db.Prepare(`SELECT EXISTS(SELECT 1 FROM processed WHERE digest = ?)`)
+	if err != nil {
+		return nil, err
+	}
+	DB.existsProcessed = stmtExistsProcessed
+
+	stmtUpsertDoc, err := db.Prepare(upsertQuery)
+	if err != nil {
+		return nil, err
+	}
+	DB.upsertDoc = stmtUpsertDoc
+
+	return DB, nil
+}
+
+// IsProcessed checks if a digest has been processed
+func (DB *DB) IsProcessed(digest string) (bool, error) {
+	var exists bool
+	err := DB.existsProcessed.QueryRow(digest).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
+
+type Source struct {
+	Src       string `json:"src"` // source - e.g. commoncrawl
+	Id        string `json:"id"`  // id that narrows down the source - e.g. the commoncrawl dataset name
+	Timestamp string `json:"ts"`  // timestamp - the time the doc was observed
+}
+
+// returns a new Source object in string format ready for
+// insertion into db
+func NewSource(src, id, ts string) ([]byte, error) {
+
+	s := &Source{
+		Src:       src,
+		Id:        id,
+		Timestamp: ts,
+	}
+	j, err := json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+	return j, nil
+
+}
+
+func (DB *DB) AddDoc(doc *gdoc.Doc, srcName, digest string) error {
+	src, err := NewSource(srcName, doc.Provenance, doc.Timestamp)
+	if err != nil {
+		return fmt.Errorf("creating source object: %w", err)
+	}
+	links, err := json.Marshal(doc.Links)
+	if err != nil {
+		return fmt.Errorf("creating links array: %w", err)
+	}
+	images, err := json.Marshal(doc.ImageUrls)
+	if err != nil {
+		return fmt.Errorf("creating links array: %w", err)
+	}
+
+	tx, err := DB.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Stmt(DB.upsertDoc).Exec(
+		doc.Id,
+		doc.Revision,
+		src,
+		doc.Created.UTC().Format(time.RFC3339),
+		doc.PageTitle,
+		doc.OgTitle,
+		doc.OgDescription,
+		doc.OgImageUrl,
+		doc.Content,
+		links,
+		images,
+		src,
+	)
+	if err != nil {
+		return fmt.Errorf("inserting doc: %w", err)
+	}
+	_, err = tx.Stmt(DB.insertProcessed).Exec(digest)
+	if err != nil {
+		return fmt.Errorf("inserting digest %s: %w", digest, err)
+	}
+
+	return tx.Commit()
+}
---
+++ cmd/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/kmsec-uk/ccdocs/commoncrawl"
+	"github.com/kmsec-uk/ccdocs/gdoc"
+	"github.com/kmsec-uk/ccdocs/warc"
+	"golang.org/x/sync/errgroup"
+)
+
+// ParsedPayload is used to pass data safely from the concurrent
+// workers to the single-threaded DB writer.
+
+type ParsedPayload struct {
+	Doc    *gdoc.Doc
+	Digest string
+}
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), "Google Docs CommonCrawl scraper!")
+		flag.PrintDefaults()
+	}
+}
+func main() {
+
+	dbPath := flag.String("db", "data/store.db", "path to database")
+
+	collection := flag.String("c", "CC-MAIN-2026-08", "commoncrawl collection to scrape")
+	flag.Parse()
+
+	db, err := NewDB(*dbPath)
+	if err != nil {
+		log.Fatalf("init db: %v", err)
+	}
+	client := commoncrawl.NewClient()
+
+	ch, err := client.Search(*collection, "url=https://docs.google.com/document/d/*&filter==status:200")
+
+	if err != nil {
+		log.Fatalf("running search : %v", err)
+	}
+	writerDone := make(chan struct{})
+	resultsCh := make(chan ParsedPayload, 50)
+	go func() {
+		for p := range resultsCh {
+			if err = db.AddDoc(p.Doc, "commoncrawl", p.Digest); err != nil {
+				log.Printf("error: inserting doc: %v", err)
+			}
+		}
+		close(writerDone)
+	}()
+	// reuse the client from the commoncrawl default Client
+	httpClient := client.HTTPClient()
+
+	g := new(errgroup.Group)
+	g.SetLimit(5)
+	// count := 0
+	for res := range ch {
+		// if count == 20 {
+		// 	break
+		// }
+		// count += 1
+		if err := res.Error; err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+		// skip Google Docs export endpoints
+		if strings.Contains(res.Record.URL, "/export") {
+			continue
+		}
+		// exclute non-utf0
+		if res.Record.Encoding != "UTF-8" {
+			continue
+		}
+		// exclude non text/html response
+		if res.Record.MimeDetected != "text/html" {
+			continue
+		}
+		exists, err := db.IsProcessed(res.Record.Digest)
+		if err != nil {
+			log.Printf("error: checking digest in DB: %v", err)
+			continue
+		}
+		if exists {
+			// skip
+			continue
+		}
+		g.Go(func() error {
+			// grab unique doc id
+			m := gdoc.GoogleDocIdRegex.FindStringSubmatch(res.Record.URL)
+			if m == nil || len(m) != 2 {
+				return nil
+			}
+			doc := gdoc.NewDoc(m[1]).WithProvenance(*collection)
+
+			reader, err := res.Record.FetchWARCItem(httpClient)
+			if err != nil {
+				log.Printf("error: doc %s: fetching %s: %v\n", doc.Id, res.Record.Filename, err)
+				return nil
+			}
+			err = warc.ParseGzippedWarcGDoc(reader, doc)
+			if err != nil {
+				log.Printf("error: doc %s: parsing gzipped html stream: %v\n", doc.Id, err)
+				return nil
+			}
+			resultsCh <- ParsedPayload{doc, res.Record.Digest}
+			return nil
+		})
+
+	}
+
+	g.Wait()
+	close(resultsCh)
+	<-writerDone
+	log.Println("finished processing")
+}
